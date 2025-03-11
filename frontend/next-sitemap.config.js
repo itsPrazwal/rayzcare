c728e9179f14ede51141971e6afb9434bea29263
Prazwal Malakar <its.prazwal@gmail.com>
2025-03-11 11:44:15 +1100

    Fix: Updates sitemap config

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://lgdr.com',
+  siteUrl: process.env.SITE_URL || 'https://rayzscare.com.au',
   generateRobotsTxt: true,
 }
